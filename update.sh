add23a437da070040dafa6e3d8c18495218e4202
cabbaage <35805885+cabbaage@users.noreply.github.com>
2019-02-03 18:51:50 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #！/bin/bash 
 ps_se=$(ps -ef | grep se.rb | grep ruby)
-if [! -n $ps_se]
+if [ -n "$ps_se"]
 then
 	pid_se=$(expr "$(ps -ef |grep ruby |grep se.rb)"  : '^\w*\s*\([0-9]*\)\s*')
-	echo $pid_se
+	res_kill_se=$(killed $pid_se)
 fi
 echo "se.rb had been killed"
