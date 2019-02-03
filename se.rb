dd56e6be7eb78d0aaf3763a1752575df5bd6ddb7
cabbaage <35805885+cabbaage@users.noreply.github.com>
2019-02-03 13:16:31 +0800

    add mult_thread
    pls

--- se.rb
+++ se.rb
@@ -1,14 +1,17 @@
 require 'socket'      # Sockets 是标准库
-
+require "thread"
 port = 2000
  
 s = TCPServer.open(port)
 puts "listining : #{port}" 
 loop {
 	puts "waiting for client"
+	Thread.new(s.accept) do |client|
 	client = s.accept
+	client.puts "#{Thread.currect}"
 	client.puts "successfully connect to me"
 	client.puts(Time.now)
 	#puts client.gets 
 	client.close
+	end 
 }
