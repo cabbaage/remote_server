7e2d14c5089aadf4b279843759787ddba593152f
cabbaage <35805885+cabbaage@users.noreply.github.com>
2019-02-03 17:16:55 +0800

    add kill pid func
    pls

--- update.sh
+++ update.sh
@@ -1,3 +1,10 @@
 #！/bin/bash 
-ps_se='ps -ef | grep se.rb | grep ruby'
-echo $ps_se
+ps_se=$(ps -ef | grep se.rb | grep ruby)
+if [ -n $ps_se]
+then
+	$ps_se =~ '^(\w*)\s*(\d*)\s*(\d*)'
+	echo $1
+	echo $2
+	echo $3
+fi
+echo "se.rb had been killed"
