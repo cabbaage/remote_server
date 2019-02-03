5a8904ef54f370f5e2d9c81e0e4462f15478137e
cabbaage <35805885+cabbaage@users.noreply.github.com>
2019-02-03 13:48:00 +0800

    fixed a bug
    pls

--- se.rb
+++ se.rb
@@ -6,8 +6,8 @@ s = TCPServer.open(port)
 puts "listining : #{port}" 
 loop {
 	puts "waiting for client"
+	puts "#{Time.now}"
 	Thread.new(s.accept) do |client|
-	client = s.accept
 	client.puts "#{Thread.currect}"
 	client.puts "successfully connect to me"
 	client.puts(Time.now)
