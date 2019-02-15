d556c1bf24cc4ea9e8fc11ee1f17868d48fa06b8
cabbaage <35805885+cabbaage@users.noreply.github.com>
2019-02-15 17:38:08 +0800

    Update https.rb

--- https.rb
+++ https.rb
@@ -1,14 +1,14 @@
-require 'webrick'
-
+require "webrick"
 class MyServlet < WEBrick::HTTPServlet::AbstractServlet
-def do_GET(request, response)
-response.status = 200
-response.content_type = "text/plain"
-response.body = "Hello, world!"
-end
+  def do_GET(request, response)
+    response.status = 200
+    response.content_type = "text/plain"
+	res = gets
+    response.body = "#{res}"
+  end
 end
 
 server = WEBrick::HTTPServer.new(:Port => 1234)
 server.mount "/", MyServlet
 trap("INT"){ server.shutdown }
-server.start 
+server.start
